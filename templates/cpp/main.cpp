c643ae35c788a79abe4e120f671f85e23082d7ab
saa01230_mac <kis1161100230@gmail.com>
2020-03-01 15:05:03 +0900

    delete boost(not installed)

--- templates/cpp/main.cpp
+++ templates/cpp/main.cpp
@@ -1,6 +1,4 @@
 #include <bits/stdc++.h>
-#include <boost/range/irange.hpp>
-using boost::irange;
 using namespace std;
 typedef long long ll;
 
@@ -8,9 +6,6 @@ typedef long long ll;
 #define rep1(i, s, n) for (int i = s; i < (int)(n); i++)
 #define inpv(i, n, v) rep(i,n) do {int tmp; cin>>tmp; v.push_back(tmp);} while(0)
 #define outv(i,n,v) rep(i,n) cout << v[i] << endl
-/*
-for (int i : irange(0, n))
-*/
 
 void Main() {
     cin >> ;
